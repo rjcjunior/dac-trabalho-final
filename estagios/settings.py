bddcd36ee45353f9ac1508b41b1e94336c546952
Wallace Coelho <wac.almeida@gmail.com>
2018-06-23 13:45:57 -0300

    replace dj-static for whitenoise

--- estagios/settings.py
+++ estagios/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'estagios', 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
