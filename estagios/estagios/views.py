be05952d544e43a719da69f396a94ccb81173ac4
Wallace Coelho <wac.almeida@gmail.com>
2018-07-07 14:31:41 -0300

    display company name or username on home

--- estagios/estagios/views.py
+++ estagios/estagios/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, redirect
+from ..core.models import Company
+
+
+def getCompany(user):
+    try:
+        company = Company.objects.get(user=user)
+        if company:
+            return company
+    except:
+        return False
 
 
 def home(request):
     if request.user.is_authenticated:
-        return render(request, 'estagios/templates/home.html')
+        displayname = request.user.get_full_name()
+
+        company = getCompany(request.user)
+        if company:
+            displayname = company.company_name
+
+        context = {
+            "displayname": displayname
+        }
+        return render(request, 'estagios/templates/home.html', context)
     else:
         return redirect('login')
