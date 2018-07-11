824d8bf8537b0236020433628ac6c6b5c15c66d1
rjcjunior <rjcjunior@hotmail.com>
2018-07-10 22:48:30 -0300

    View das Vagas

--- estagios/estagios/urls.py
+++ estagios/estagios/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('editStudent', editStudent, name='editStudent'),
     path('parabens', parabens, name='parabens'),
-    path('vaga', vaga, name='vaga'),
+    path('vaga/(\d{4})/$', vaga, name='vaga'),
     path('candidatura', candidatura, name='candidatura'),
     path('create-job', create_job, name='create-job'),
     path('', home, name='home'),
